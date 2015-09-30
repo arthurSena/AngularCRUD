6ee157bd51508011d8589cf15cf12867ef0f8840
arthur <arthur.senaufcg@gmail.com>
2015-09-30 15:33:37 -0300

    Main functions are ready

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -31,32 +31,46 @@ app.controller('MainController', ['$scope','dataService',function($scope,dataSer
 				};
    		 }
 
+   		 $scope.editContact = function(contact){
+   		 	$scope.temp = {
+   		 			id:contact.id,
+   		 			first_name:contact.first_name,
+   		 			last_name:contact.last_name,
+   		 			email:contact.email,
+   		 			country:contact.country
+   		 	}
+   		 }
+
+   		 $scope.editAndAdd = function(){
+   		 	for (var i = $scope.contacts.length - 1; i >= 0; i--) {
+   		 		if($scope.contacts[i].id == $scope.temp.id){
+   		 			$scope.contacts[i] = $scope.temp;
+   		 		}
+   		 	};
+
+   		 	$scope.temp = {
+					id:-1,
+   		 			first_name:"",
+   		 			last_name:"",
+   		 			email:"",
+   		 			country:""
+				};
+   		 }
+
+
+   		 $scope.removeContact = function(contact){
+
+   		 	for (var i = $scope.contacts.length - 1; i >= 0; i--) {
+   		 		if($scope.contacts[i] == contact){
+   		 			$scope.contacts.splice(i,1);
+   		 		}
+
+   		 	};
+   		 }
+
    		 $scope.selectedRow = null;  // initialize our variable to null
   		 $scope.setClickedRow = function(index){  //function that sets the value of selectedRow to current index
      		$scope.selectedRow = index;
   		}
 
-		// $scope.loadData = function(){
-		// 	$scope.numLimit += 10;
-		// 	if ($scope.numLimit > 10) {
-		// 		var temp = 0;
-		// 		for (var i = $scope.numLimit; i < $scope.numLimit + 10 ; i++){
-		// 			$scope.contactsToShow[temp] = $scope.contacts[i];
-		// 			temp++;
-		// 		}
-		// 	}
-		// }
-
-		// $scope.nextPage = function(){
-		// 	numLimit += 10;
-		// }
-
 }]);
-
-// app.controller('MainController', ['$scope', 'forecast', function($scope, forecast) { 
-//  forecast.success(function(data) { 
-//     $scope.fiveDay = data; 
-//   });
-  
-  
-// }]);
